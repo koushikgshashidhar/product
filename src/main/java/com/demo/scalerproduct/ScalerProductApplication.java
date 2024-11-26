260c0c6dbfb7fe38bf03dccbee3cb6c01d2bdf23
koushik <koushikshashidhar@gmail.com>
2024-11-26 15:26:20 +0530

    created springBoot project

---
+++ src/main/java/com/demo/scalerproduct/ScalerProductApplication.java
@@ -0,0 +1,13 @@
+package com.demo.scalerproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/scalerproduct/ScalerProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.scalerproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
